5050d312c3ac608790cbde0f5e367912b690fe1f
Ricky <ricky@yewdofu.xyz>
2026-04-02 19:46:26 +0900

    exclude production build from .gitignore

---
+++ extension/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const example = (nodecg) => {
+  nodecg.log.info("example extension\u5B9F\u884C");
+};
+
+var index = (nodecg) => {
+  example(nodecg);
+};
+
+module.exports = index;
+//# sourceMappingURL=index.js.map
